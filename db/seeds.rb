043b79c1147cc3e0a478c1c22bda7a4716ff108a
Steve Loar <sjl7909@gmail.com>
2014-08-14 16:15:19 -0500

    Working on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Tour.delete_all
+Poi.delete_all
+
+admin = User.create( username: 'admin', email: 'admin@gmail.com', password: 'password', password_confirmation: 'password')
+
+Tour.create(name: 'Architecture Tour I', creator_id: admin.id, zoom: 17, 
+  center: {lat: 41.8887255, lng: -87.6270584})
+
+tour1 = admin.tours.first
+
+# create some POIs for out tours
+Poi.create( name: 'Tribune Tower', tour_id: tour1.id,
+          description: 'The Tribune Tower is a neo-Gothic building located at 435 North Michigan Avenue in Chicago, Illinois, USA. It is the home of the Chicago Tribune and Tribune Company.', 
+          image: 'assets/images/tribunetower2.jpg',
+          location: lat: 41.89046592218648, lng: -87.62356281280518})
+
