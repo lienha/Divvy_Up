3ce89cfc6851260123f2f0ebb60e559b8abbc330
Jennifer Oseitutu <JennieO23@gmail.com>
2014-08-14 16:17:13 -0500

    starting the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ DivvyUp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  get '/signup', to: 'welcome#index'
+  # get '/login', to: 'sessions#new'
+  # get '/logout', to: 'sessions#destroy'
+
   root 'welcome#index'
 
   # Example of regular route:
