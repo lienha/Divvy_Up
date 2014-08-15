27b66101bdec7f2b6a8483ffa13503158175e913
Steve Loar <sjl7909@gmail.com>
2014-08-14 19:57:58 -0500

    more tour poi

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,3 +43,18 @@ Poi.create( name: 'Tribune Tower', tour_id: tour1.id,
           description: 'The Tribune Tower is a neo-Gothic building located at 435 North Michigan Avenue in Chicago, Illinois, USA. It is the home of the Chicago Tribune and Tribune Company.', 
           image: 'assets/images/tribunetower2.jpg',
           location: {lat: 41.89046592218648, lng: -87.62356281280518}.to_json)
+Poi.create( name: 'Wrigley Building',
+          description: "The Wrigley Building is a skyscraper located directly across Michigan Avenue from the Tribune Tower on the Magnificent Mile. It was built to house the corporate headquarters of the Wrigley Company.",
+          image: "assets/images/wrigley.jpg",
+          location: {lat: 41.889379709685485, lng: -87.62463569641113}.to_json)
+Poi.create( name: 'Trump Tower',
+          description: "The Wrigley Building is a skyscraper located directly across Michigan Avenue from the Tribune Tower on the Magnificent Mile. It was built to house the corporate headquarters of the Wrigley Company.",
+          image: "assets/images/wrigley.jpg",
+          location: {lat: 41.889379709685485, lng: -87.62463569641113}.to_json)
+Poi.create( name: 'Marina City',
+          description: "Marina City is a mixed-use residential/commercial building complex that occupies an entire city block on State Street in Chicago, Illinois. The complex consists of two corncob-shaped, 587-foot (179 m), 65-story towers.",
+          image: "assets/images/marina_city.jpg",
+          location: {lat: 41.889379709685485, lng: -87.62463569641113}.to_json)
+
+
+
