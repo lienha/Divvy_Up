6c8141700a187e049d1ab57397904607499a59ea
ItsYou <hman87@outlook.com>
2014-08-15 17:06:03 -0500

    maps working, but no marker

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,22 +17,22 @@ end
 admin = User.create( username: 'admin', email: 'admin@gmail.com', password: 'password', password_confirmation: 'password')
 
 
-Tour.create(name: 'Architecture Tour', creator_id: admin.id, zoom: 17, 
+Tour.create(name: 'Architecture Tour', creator_id: admin.id, zoom: 17,
+  center: JSON.generate({lat: 41.8887255, lng: -87.6270584}.to_json)
+
+Tour.create(name: 'Party Tour', creator_id: 1, zoom: 17,
   center: {lat: 41.8887255, lng: -87.6270584}.to_json)
 
-Tour.create(name: 'Party Tour', creator_id: 1, zoom: 17, 
+Tour.create(name: 'Crime and Mob Tour', creator_id: 2, zoom: 17,
   center: {lat: 41.8887255, lng: -87.6270584}.to_json)
 
-Tour.create(name: 'Crime and Mob Tour', creator_id: 2, zoom: 17, 
+Tour.create(name: 'Film Tour', creator_id: 3, zoom: 17,
   center: {lat: 41.8887255, lng: -87.6270584}.to_json)
 
-Tour.create(name: 'Film Tour', creator_id: 3, zoom: 17, 
+Tour.create(name: 'City Tour', creator_id: 4, zoom: 17,
   center: {lat: 41.8887255, lng: -87.6270584}.to_json)
 
-Tour.create(name: 'City Tour', creator_id: 4, zoom: 17, 
-  center: {lat: 41.8887255, lng: -87.6270584}.to_json)
-
-Tour.create(name: 'Grand Tour', creator_id: 5, zoom: 17, 
+Tour.create(name: 'Grand Tour', creator_id: 5, zoom: 17,
   center: {lat: 41.8887255, lng: -87.6270584}.to_json)
 
 
@@ -40,7 +40,7 @@ tour1 = admin.tours.first
 
 # create some POIs for our tours
 Poi.create( name: 'Tribune Tower', tour_id: tour1.id,
-          description: 'The Tribune Tower is a neo-Gothic building located at 435 North Michigan Avenue in Chicago, Illinois, USA. It is the home of the Chicago Tribune and Tribune Company.', 
+          description: 'The Tribune Tower is a neo-Gothic building located at 435 North Michigan Avenue in Chicago, Illinois, USA. It is the home of the Chicago Tribune and Tribune Company.',
           image: 'assets/images/tribunetower2.jpg',
           location: {lat: 41.89046592218648, lng: -87.62356281280518}.to_json)
 Poi.create( name: 'Wrigley Building', tour_id: tour1.id,
