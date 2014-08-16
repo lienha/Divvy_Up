eea0dfe7a623981ffdbae09adf449d7de36309ed
ItsYou <hman87@outlook.com>
2014-08-16 15:44:56 -0500

    controllers for poi

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140816030054) do
     t.string   "remember_token"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
