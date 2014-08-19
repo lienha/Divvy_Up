3e267790830d0e5743880e8d74fd8519b4f61a21
ItsYou <hman87@outlook.com>
2014-08-19 18:57:15 -0500

    refactoring..

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,21 @@
 class CommentsController < ApplicationController
 	# content_type :json
-
   def create
-    comment = Comment.create(poi_id: params[:poi_id], user_id: current_user.id, text: strip_newlines)
-    redirect_to (:back)
+    @poi = params[:poi_id]
+    comment = Comment.create(poi_id: @poi, user_id: current_user.id, text: strip_newlines)
+    curr_poi = Poi.find(@poi.to_i)
+    comments = curr_poi.comments.all
+
+    respond_to do |format|
+      format.js
+    end
     #   respond_to do |format|
     #   msg = { :status => "ok", :message => "Success!", :id => comment.id }
     #   format.json  { render :json => msg } # don't do msg.to_json
     #   end
 		# head :ok
 		# ajax
-	end
+  end
 
   private
     def strip_newlines
--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -3,7 +3,6 @@ class ToursController < ApplicationController
     @tours = Tour.all
     render 'index'
   end
-
   def show
     if Tour.find_by(id: params[:id])
       @tour = Tour.find(params[:id])
